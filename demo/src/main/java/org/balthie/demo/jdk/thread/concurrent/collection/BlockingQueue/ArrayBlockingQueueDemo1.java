175e008b1c7209ab35049b7167fbda82ae8bdadc
balthie <balthie@126.com>
2015-11-03 13:57:59 +0800

    12

--- demo/src/main/java/org/balthie/demo/jdk/thread/concurrent/collection/BlockingQueue/ArrayBlockingQueueDemo1.java
+++ demo/src/main/java/org/balthie/demo/jdk/thread/concurrent/collection/BlockingQueue/ArrayBlockingQueueDemo1.java
@@ -1,4 +1,4 @@
-package org.balthie.demo.jdk.thread.concurrent.collection.ArrayBlockingQueue;
+package org.balthie.demo.jdk.thread.concurrent.collection.BlockingQueue;
 
 //ArrayBlockingQueue是用循环数组，LinkedBlockingQueue用所谓的双锁算法避免读写冲突， LinkedBlockingDQueue用了读写互斥
 public class ArrayBlockingQueueDemo1 {
