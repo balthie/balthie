24d08de1d7840e4af26a0eb14fe0fdee21e14694
lenovo-2 <lenovo-2@balthie>
2016-01-14 12:25:39 +0800

    Signed-off-by: lenovo-2 <lenovo-2@balthie>

--- demo/src/main/java/org/balthie/demo/my/clazz/inner/Main.java
+++ demo/src/main/java/org/balthie/demo/my/clazz/inner/Main.java
@@ -9,9 +9,32 @@ package org.balthie.demo.my.clazz.inner;
  * @author：balthie + 10050
  * @createtime ： 2015年3月9日 上午11:41:08
  * @description TODO 一句话描述
- * @since version 初始于版本 TODO 
+ * @since version 初始于版本 TODO
  */
 public class Main
 {
+    public static void main(String[] args)
+    {
+        Main m = new Main();
+        m.anonymousExample();
+    }
+    
+    public void anonymousExample()
+    {
+        String nonFinalVariable = "Non Final Example";
+        String variable = "Outer Method Variable";
+        new Thread(new Runnable() {
+            String variable = "Runnable Class Member";
+            
+            public void run()
+            {
+                String variable = "Run Method Variable";
+                // Below line gives compilation error.
+                // System.out.println("->" + nonFinalVariable);
+                System.out.println("->" + variable);
+                System.out.println("->" + this.variable);
+            }
+        }).start();
+    }
     
 }
